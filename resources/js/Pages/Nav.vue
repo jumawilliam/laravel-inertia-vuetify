43d68d4693c901e3161db6709fa8fa20c03363fb
laraphant <codersbase1@gmail.com>
2024-10-10 05:01:12 +0300

    vuetify

---
+++ resources/js/Pages/Nav.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+<template>
+    <v-app>
+    <v-app-bar color="primary" dark>
+      <v-toolbar-title>My App</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <p>Welcome to the app!</p>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
